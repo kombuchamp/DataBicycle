c5668db909f35112135d3e60c9754079e5749fa5
Konstantin Belousov <kombuchamp@gmail.com>
2018-10-01 15:05:59 +0300

    data obtainer refactorings

--- Core/DataObtainer.cs
+++ Core/DataObtainer.cs
@@ -13,34 +13,23 @@ namespace DataBicycle.Core
             this.connection = connection;
         }
 
-
-
-        // Коллекция Dictionary. Содержит пары key - value
-        // для сопоставления индексов листбокса и ID в таблице БД
-        // key - index, value - ID.
         static Dictionary<int, int> IndexIDdict = new Dictionary<int, int>();
-
 
         public void SearchAndFillList(string query, ListBox listbox)
         {
-            // Безопасная вставка значения AddWithValue не работает в данном контексте (почему - неизвестно)
-            // Используем конкатинацию строк
-
-            // Пытаемся избежать sql-injection
+            // Trying to avoid an sql-injection
             query = query.Replace(';', ' ');
             query = query.Replace('"', ' ');
             query = query.Replace('\'', ' ');
             query = query.Replace('/', ' ');
 
-
-
             SqlCommand cmdSelectByQuery = new SqlCommand();
             cmdSelectByQuery.Connection = connection;
-            cmdSelectByQuery.CommandText = "SELECT ID, WName FROM [Table] WHERE WName LIKE '%" + query + "%'";// @WName 
-            //cmdSelectByQuery.Parameters.AddWithValue("WName", query); // не работает
-
+            cmdSelectByQuery.CommandText = "SELECT ID, WName " +
+                                           "FROM [Table] " +
+                                           "WHERE WName LIKE '%" + query + "%'";// @WName 
+            //cmdSelectByQuery.Parameters.AddWithValue("WName", query); // won't work. Using concatination here.
             FillList(listbox, cmdSelectByQuery);
-
         }
 
         public void FillList(ListBox listbox, SqlCommand command = null)
@@ -61,7 +50,7 @@ namespace DataBicycle.Core
             while (reader.Read())
             {
                 int id = (int)reader[0];
-                IndexIDdict.Add( listbox.Items.Count , id);
+                IndexIDdict.Add(listbox.Items.Count, id);
 
                 string name = reader.GetString(1);
                 
@@ -71,8 +60,6 @@ namespace DataBicycle.Core
                
         }
 
-
-        // Возвращает ID, принимает индекс листбокса
         public static int IndexToID(int index)
         {
             return IndexIDdict[index];
